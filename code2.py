3a060b4da3f6b94a8ab69a82663cfdab4f0d60a3
William Mburu <69644126+SirWilliam254@users.noreply.github.com>
2023-02-05 12:15:14 +0300

    Create code2.py

---
+++ code2.py
@@ -0,0 +1,47 @@
+# importing data
+import arch.data.sp500
+
+data = arch.data.sp500.load()
+market = data["Adj Close"]
+returns = 100 * market.pct_change().dropna()
+
+# defining model
+am = arch_model(returns, vol="Garch", p=1, o=0, q=1, dist="skewt")
+res = am.fit(disp="off", last_obs="2017-12-31")
+
+# forecasting
+forecasts = res.forecast(start="2018-1-1", reindex=False)
+cond_mean = forecasts.mean["2018":]
+cond_var = forecasts.variance["2018":]
+q = am.distribution.ppf([0.01, 0.05], res.params[-2:])
+print(q)
+
+# VAR 
+value_at_risk = -cond_mean.values - np.sqrt(cond_var).values * q[None, :]
+value_at_risk = pd.DataFrame(value_at_risk, columns=["1%", "5%"], index=cond_var.index)
+ax = value_at_risk.plot(legend=False)
+xl = ax.set_xlim(value_at_risk.index[0], value_at_risk.index[-1])
+rets_2018 = returns["2018":].copy()
+rets_2018.name = "S&P 500 Return"
+c = []
+for idx in value_at_risk.index:
+    if rets_2018[idx] > -value_at_risk.loc[idx, "5%"]:
+        c.append("#000000")
+    elif rets_2018[idx] < -value_at_risk.loc[idx, "1%"]:
+        c.append("#BB0000")
+    else:
+        c.append("#BB00BB")
+c = np.array(c, dtype="object")
+labels = {"#BB0000": "1% Exceedence", "#BB00BB": "5% Exceedence", "#000000": "No Exceedence",}
+markers = {"#BB0000": "x", "#BB00BB": "s", "#000000": "o"}
+for color in np.unique(c):
+    sel = c == color
+    ax.scatter(
+        rets_2018.index[sel],
+        -rets_2018.loc[sel],
+        marker=markers[color],
+        c=c[sel],
+        label=labels[color],
+    )
+ax.set_title("Parametric VaR")
+leg = ax.legend(frameon=False, ncol=3)
